db6398a8b62edaa3ef6ed8311e1a09a89361ffe7
Hugo Camilo Rendon Lozano <hrvega14@hotmail.com>
2025-07-12 03:58:02 -0500

    Agregando AppProvider

---
+++ src/context/AppProvider.jsx
@@ -0,0 +1,43 @@
+import { createContext, useReducer } from "react";
+
+const AppContext = createContext();
+const { Provider, Consumer } = AppContext;
+
+const initialState = {
+  user: JSON.parse(localStorage.getItem("user")),
+  token: JSON.parse(localStorage.getItem("access_token")),
+  group_selected: JSON.parse(localStorage.getItem("group_selected")),
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_USER":
+      localStorage.setItem("user", JSON.stringify(action.data));
+      return { ...state, user: action.data };
+    case "SET_TOKEN":
+      localStorage.setItem("access_token", JSON.stringify(action.data));
+      return { ...state, token: action.data };
+    case "SET_GROUP_SELECTED":
+      localStorage.setItem("group_selected", JSON.stringify(action.data));
+      return { ...state, group_selected: action.data };
+    default:
+      return { ...state };
+  }
+};
+
+const AppProvider = ({ children }) => {
+  const [state, setReducer] = useReducer(reducer, initialState);
+
+  const setApp = (type, data) => {
+    setReducer({ type: type, data });
+  };
+
+ 
+  const value = {
+    ...state,
+    setApp
+  };
+
+  return <Provider value={value}>{children}</Provider>;
+};
+export { Consumer as AppConsumer, AppContext, AppProvider };
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.jsx";
+import { AppProvider } from "./context/AppProvider.jsx";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +12,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <AppProvider>
+          <App />
+        </AppProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
