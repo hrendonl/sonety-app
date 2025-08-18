6ef0ded7e8e4d7b9c295f60efd556fa03abdd423
Hugo Camilo Rendon Lozano <hrvega14@hotmail.com>
2025-08-18 16:13:14 -0500

    Uso de logo naranja mientras se define logo

--- src/components/layout/NavMain.jsx
+++ src/components/layout/NavMain.jsx
@@ -8,7 +8,7 @@ import {
   MdPeopleOutline,
   MdOutlineSettings
 } from "react-icons/md";
-import Logo from "../../assets/images/logo_azulin.png";
+import Logo from "../../assets/images/logo_prev.png";
 import { Link, NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { AppContext } from "../../context/AppProvider";
